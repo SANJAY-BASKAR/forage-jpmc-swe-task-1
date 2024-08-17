3efa7fb74acddebb34fb053d172628dc9fbfaa4c
SANJAY BASKAR <ssssantech@gmail.com>
2024-08-17 10:57:14 +0530

    changes to patch file added

--- client3.py
+++ client3.py
@@ -21,6 +21,7 @@
 import json
 import random
 import urllib.request
+import time
 
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
@@ -35,14 +36,17 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2;
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return
+        # avoids throwing error
+    return price_a / price_b
 
 
 # Main
@@ -52,8 +56,13 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        value = {} # initiate to store different stocks
         for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+
+            value[stock] = price
+
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+# focused line    # need two different STOCKS for ratio
+        print("Ratio %s" % (getRatio(value['ABC'], value['DEF'])))
